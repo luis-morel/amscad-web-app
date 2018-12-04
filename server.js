932e025e3fe642500d98fbfb4e5007d0fa8dd883
LMorel77 <lmorel77@aol.com>
2018-12-03 23:22:21 -0500

    Some tweaking

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ const PORT = process.env.PORT || 5000;
 // Use ...sync({force: true})... to drop and recreate all tables - WILL WIPE ALL DATA!!!
 db.sequelize.sync().then(function () {
   app.listen(PORT, function () {
-    console.log(`Express web server listening on port: ${PORT}!`);
+    console.log(`\nExpress web server listening on port: ${PORT}!\n`);
   });
 });
